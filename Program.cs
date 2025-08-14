7c7cf9fda00c11be9074675043d413fc2bf92a7d
nathanaelgs <nathanael.goncalves@sga.pucminas.br>
2025-08-14 14:02:09 +0000

    as

--- Program.cs
+++ Program.cs
@@ -16,7 +16,6 @@ class Program {
         {
             v = Console.ReadLine().Split(" ");
             vdouble[0] = double.Parse(v[0]);
-            Console.WriteLine(vdouble[0]);
             vdouble[1] = double.Parse(v[1]);
             if (vdouble[1] == 1000) { valorperc = vdouble[0]; }
             else { valorperc = vdouble[0] + (((1000 / 1000) - (vdouble[1] / 1000)) * vdouble[1]); }
