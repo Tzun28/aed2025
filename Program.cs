bb4851204e589aaf198ce6cc68bfb5b8cf665c9a
nathanaelgs <nathanael.goncalves@sga.pucminas.br>
2025-09-03 21:59:41 +0000

    save

--- Program.cs
+++ Program.cs
@@ -1,32 +1,39 @@
 ﻿using System;
 
-class Program {
+class Program
+{
     public static void Main()
     {
-
-        int n = int.Parse(Console.ReadLine());
-        string[] v = new string[2];
-        double[] vdouble = new double[2];
-        double valorperc, valorcxb;
-        v = Console.ReadLine().Split(" ");
-        vdouble[0] = double.Parse(v[0]);
-        vdouble[1] = double.Parse(v[1]);
-        valorcxb = double.Parse(v[0])*100;
-        if (vdouble[1] == 1000) { valorperc = vdouble[0]; }
-        else { valorperc = vdouble[0] / (vdouble[1] / 1000); }
-        if (valorperc < valorcxb) { valorcxb = valorperc; }
-        for (int a = 1; a < n; a++)//N supermercados
+        string[] res = Console.ReadLine().Split(" ");
+        int posic = int.Parse(res[0]), rep = int.Parse(res[1]);
+        int[] vetor = new int[posic]; foreach (int posicao in vetor){ vetor[posicao] = 0; }
+        int valorI, valorV;
+        for (int a = rep; a < rep; a++)
         {
-            v = Console.ReadLine().Split(" ");
-            vdouble[0] = double.Parse(v[0]);
-            vdouble[1] = double.Parse(v[1]);
-            if (vdouble[1] == 1000) { valorperc = vdouble[0]; }
-            else { valorperc = vdouble[0] / (vdouble[1] / 1000); }
-            if (valorperc < valorcxb) { valorcxb = valorperc; }
-
+            string[] respondedor = Console.ReadLine().Split(" ");
+            int verifica = int.Parse(respondedor[0]);
+            if (verifica == 3)
+            {
+                valorI = int.Parse(respondedor[1]);
+                Console.WriteLine(vetor[valorI]);
+            }
+            else if (verifica == 1)
+            {
+                valorI = int.Parse(respondedor[1]);
+                valorV = int.Parse(respondedor[2]);
+                
+            }
+            else
+            {
+                valorI = int.Parse(respondedor[1]);
+                valorV = int.Parse(respondedor[2]);
+            }
+                
+                
+                
         }
-        Console.WriteLine($"{valorcxb:F2}");
-
         
+
+
     }
 }
