56d48c723fb4a979899eb7c694a4c145295a6b8d
nathanaelgs <nathanael.goncalves@sga.pucminas.br>
2025-09-17 22:20:55 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,36 +4,15 @@ class Program
 {
     public static void Main()
     {
-        string[] res = Console.ReadLine().Split(" ");
-        int posic = int.Parse(res[0]), rep = int.Parse(res[1]);
-        int[] vetor = new int[posic]; foreach (int posicao in vetor){ vetor[posicao] = 0; }
-        int valorI, valorV;
-        for (int a = rep; a < rep; a++)
-        {
-            string[] respondedor = Console.ReadLine().Split(" ");
-            int verifica = int.Parse(respondedor[0]);
-            if (verifica == 3)
-            {
-                valorI = int.Parse(respondedor[1]);
-                Console.WriteLine(vetor[valorI]);
-            }
-            else if (verifica == 1)
-            {
-                valorI = int.Parse(respondedor[1]);
-                valorV = int.Parse(respondedor[2]);
-                
-            }
-            else
-            {
-                valorI = int.Parse(respondedor[1]);
-                valorV = int.Parse(respondedor[2]);
-            }
-                
-                
-                
-        }
-        
 
+        /*retângulo de largura L metros e comprimento C metros*/
+        int lar = int.Parse(Console.ReadLine());
+        int com = int.Parse(Console.ReadLine());
+        int tipoum = (lar * com) + ((lar - 1) * (com - 1));
+        int tipodois = ((com - 1) * 2) + ((lar - 1) * 2);
+        Console.WriteLine(tipoum);
+        Console.WriteLine(tipodois);
+        
 
     }
 }
