ba2767ea6658661e7a6f5a7db986117f477b5996
nathanaelgs <nathanael.goncalves@sga.pucminas.br>
2025-09-29 20:56:30 +0000

    va

--- obj/Debug/net8.0/aed2025.AssemblyInfo.cs
+++ obj/Debug/net8.0/aed2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aed2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d43627cb480a68d7b0ec1491596398b94fd6565d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abebae2afadca8714a5633f2824594a51c0e8602")]
 [assembly: System.Reflection.AssemblyProductAttribute("aed2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aed2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
