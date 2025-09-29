d43627cb480a68d7b0ec1491596398b94fd6565d
nathanaelgs <nathanael.goncalves@sga.pucminas.br>
2025-09-29 20:52:49 +0000

    Junta tudo

--- Program.cs
+++ Program.cs
@@ -1,38 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-class Program{
-    public static void Main(){
-        int rep = int.Parse(Console.ReadLine());
-        string[] valores = Console.ReadLine().Split(" ");
-        
-        int a = int.Parse(valores[0]);
-        int b = int.Parse(valores[1]);
-        Executa(a, b, rep);
-
-    }
-        
-    public static void Executa(int a, int b, int z){
-        string[] valores = Console.ReadLine().Split(" ");
-        int x = int.Parse(valores[0]);
-        int y = int.Parse(valores[1]);
-        int d = 1;
-        int mdiv = 1;
-        MDC(x,y,d, mdiv);
-        Console.WriteLine(mdiv);
-        
-        if (z != 0 && z > 0) { }
-        else { Executa(x, y, z -= 1); }
-    }
-    public static void MDC(int varium, int varidois, int divisor, int maxdivisor)
-    {
-        if ((divisor % varium == 0) && (divisor % varidois == 0) && (divisor > maxdivisor))
-        {
-            maxdivisor = divisor;
+    class Program{
+        public static void Main(){
+            int rep = int.Parse(Console.ReadLine());
+            Executa(--rep);
         }
-        
-        if (divisor < varium || divisor < varidois) { MDC(varium, varidois, divisor += 1, maxdivisor); }
-        
+        public static void Executa(int z){
+            string[] valores = Console.ReadLine().Split(" ");
+            int x = int.Parse(valores[0]);
+            int y = int.Parse(valores[1]);
+            int mdiv = 1;
+            MDC(x,y, ref mdiv);
+            Console.WriteLine(mdiv);
+            mdiv = 1;
+            if (z != 0 && z > 0) {Executa(--z); }
+        }
+    public static void MDC(int varium, int varidois, ref int maxdivisor)
+    {
+        if (varidois == 0) { maxdivisor = varium; }
+        else { MDC(varidois, varium % varidois, ref maxdivisor); }
     }
-   
-    
+                    
 }
