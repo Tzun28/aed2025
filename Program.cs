08b731f74883972b2cdffd5ef85e0b553b027438
nathanaelgs <nathanael.goncalves@sga.pucminas.br>
2025-09-30 20:03:14 +0000

    sobe

--- Program.cs
+++ Program.cs
@@ -1,50 +1,141 @@
 ﻿using System;
 using System.Collections.Generic;
-class Program{
-    public static void Main(){
-        Random r = new Random();
-        List<int> Tetetetestando = new List<int>();
-        Tetetetestando.Add(r.Next(0, 100));Tetetetestando.Add(r.Next(0, 100));Tetetetestando.Add(r.Next(0, 100));
-        Tetetetestando.Add(r.Next(0, 100));Tetetetestando.Add(r.Next(0, 100));Tetetetestando.Add(r.Next(0, 100));
-        Tetetetestando.Add(r.Next(0, 100));Tetetetestando.Add(r.Next(0, 100));Tetetetestando.Add(r.Next(0, 100));
-        Console.WriteLine("Digite o início da lista");
-        int a = int.Parse(Console.ReadLine());
-        Console.WriteLine("Digite o fim da lista");
-        int b = int.Parse(Console.ReadLine());
-        List<int> Rei = Copiapartelista(Tetetetestando, a ,b);
-        Console.WriteLine(Rei.Count() + "," + Tetetetestando.Count());
-        foreach (int delta in Rei) { Console.WriteLine(delta); }
-    }
-    public static List<T> Copiapartelista<T>(List<T> Recebe, int inicio, int fim ){
-        List<T> Nvlista = new List<T>();
+class Program
+{
+    public static void Main()
+    {
+        Dictionary<char, Dictionary<char, Dictionary<char, string>>> Aminoacidos = new Dictionary<char, Dictionary<char, Dictionary<char, string>>>
+        {
+            ['U'] = new Dictionary<char, Dictionary<char, string>>
+            {
+                ['U'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Fenilalanina",
+                    ['C'] = "Fenilalanina",
+                    ['A'] = "Leucina",
+                    ['G'] = "Leucina"
+                },
+                ['C'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Serina",
+                    ['C'] = "Serina",
+                    ['A'] = "Serina",
+                    ['G'] = "Serina"
+                },
+                ['A'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Tirosina",
+                    ['C'] = "Tirosina",
+                    ['A'] = "Parada",
+                    ['G'] = "Parada"
+                },
+                ['G'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Cisteína",
+                    ['C'] = "Cisteína",
+                    ['A'] = "Parada",
+                    ['G'] = "Triptofano"
+                }
+            },
+            ['C'] = new Dictionary<char, Dictionary<char, string>>
+            {
+                ['U'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Leucina",
+                    ['C'] = "Leucina",
+                    ['A'] = "Leucina",
+                    ['G'] = "Leucina"
+                },
+                ['C'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Prolina",
+                    ['C'] = "Prolina",
+                    ['A'] = "Prolina",
+                    ['G'] = "Prolina"
+                },
+                ['A'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Hidistina",
+                    ['C'] = "Hidistina",
+                    ['A'] = "Glutamina",
+                    ['G'] = "Glutamina"
+                },
+                ['G'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Arginina",
+                    ['C'] = "Arginina",
+                    ['A'] = "Arginina",
+                    ['G'] = "Arginina"
+                }
+            },
+            ['A'] = new Dictionary<char, Dictionary<char, string>>
+            {
+                ['U'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Isoleucina",
+                    ['C'] = "Isoleucina",
+                    ['A'] = "Isoleucina",
+                    ['G'] = "Metionina"
+                },
+                ['C'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Treonina",
+                    ['C'] = "Treonina",
+                    ['A'] = "Treonina",
+                    ['G'] = "Treonina"
+                },
+                ['A'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Asparagina",
+                    ['C'] = "Asparagina",
+                    ['A'] = "Lisina",
+                    ['G'] = "Lisina"
+                },
+                ['G'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Serina",
+                    ['C'] = "Serina",
+                    ['A'] = "Arginina",
+                    ['G'] = "Arginina"
+                }
+            },
+            ['G'] = new Dictionary<char, Dictionary<char, string>>
+            {
+                ['U'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Valina",
+                    ['C'] = "Valina",
+                    ['A'] = "Valina",
+                    ['G'] = "Valina"
+                },
+                ['C'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Alanina",
+                    ['C'] = "Alanina",
+                    ['A'] = "Alanina",
+                    ['G'] = "Alanina"
+                },
+                ['A'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Aspartato",
+                    ['C'] = "Aspartato",
+                    ['A'] = "Glutamato",
+                    ['G'] = "Glutamato"
+                },
+                ['G'] = new Dictionary<char, string>
+                {
+                    ['U'] = "Glicina",
+                    ['C'] = "Glicina",
+                    ['A'] = "Glicina",
+                    ['G'] = "Glicina"
+                }
+            }
 
-        if (inicio > Recebe.Count()){ Console.WriteLine("a"); return Nvlista;}
-        else if (inicio > fim){
-            if (inicio == 0){ inicio = 1; }   
-            for (int z = fim; z <= inicio; z++)
-            {
-                Nvlista.Add(Recebe[z-1]);
-            }
-            Console.WriteLine("b");
-            return Nvlista;
-        }
-        else if (fim > Recebe.Count()){
-            if (inicio == 0) { inicio = 1; }
-            for (int z = inicio; z <= Recebe.Count(); z++)
-            {
-                Nvlista.Add(Recebe[z-1]);
-            }
-            Console.WriteLine("c");
-            return Nvlista;
-        }
-        else {   
-            if (inicio == 0){ inicio = 1; }
-            for (int z = inicio; z <= fim; z++)
-            {
-                Nvlista.Add(Recebe[z - 1]);
-            }
-            Console.WriteLine("d");
-            return Nvlista;
-        }
+        };
+    Console.WriteLine("Digite a trinca de nucleotídeos");
+        
+    }
+        
+        
     }
 }
--- obj/Debug/net8.0/aed2025.AssemblyInfo.cs
+++ obj/Debug/net8.0/aed2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aed2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9934c9dda84bf354e454611e79acc679cdb90343")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b826e37ae9463e3807d06a1a2ea32211477b0f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("aed2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aed2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
