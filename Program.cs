ed5a3350498cc1f6dd93a0445871fa7f8d48088a
nathanaelgs <nathanael.goncalves@sga.pucminas.br>
2025-10-16 19:59:37 +0000

    tentando

--- Program.cs
+++ Program.cs
@@ -4,23 +4,20 @@ class Program
     public static void Main()
     {
         int qtdpersonagens = int.Parse(Console.ReadLine());
+            List<Personagem> personagins = new List<Personagem>();
         for (int z = 0; z < qtdpersonagens; z++)
         {
             string taldopersonagem = Console.ReadLine();
-            List<string> personagins = new List<string>();
-            foreach (string caracteristica in taldopersonagem.Split(";"))
+            string nomedigitado = taldopersonagem.Split(";")[0];
+            Personagem tal = new Personagem()
             {
-                personagins.Add(caracteristica);
-            }
-
-
-            Personagem Dart = new Personagem()
-            {
-
+                
             };
+            personagins.Add(tal);
         }
     }
 }
+
 class Personagem {
     private string nome;
     private int altura;
@@ -31,4 +28,9 @@ class Personagem {
     private string anoNascimento;
     private string genero;
     private string homeworld;
+
+    public void Adiciona(string oque)
+    {
+        nome = nomedigitado;
+    }
 }
